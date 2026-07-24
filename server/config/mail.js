6cca02871035318054d3010aee1589d5d63c87fd
Ayush sharma <nitinrewa9691@gmail.com>
2026-07-24 12:42:30 +0530

    Fix mail configuration and server updates

--- server/config/mail.js
+++ server/config/mail.js
@@ -1,9 +1,6 @@
 import "dotenv/config";
 import nodemailer from "nodemailer";
 
-console.log("MAIL USER:", process.env.EMAIL_USER);
-console.log("MAIL PASS EXISTS:", !!process.env.EMAIL_PASS);
-console.log("MAIL PASS LENGTH:", process.env.EMAIL_PASS?.length);
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
